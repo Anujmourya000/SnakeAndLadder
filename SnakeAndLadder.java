fe2bf03e2a8763325c10a5828cce5c12a96cc0e8
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-27 21:59:59 +0530

    Added: UC2 problem is added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,4 +1,13 @@
 
 public class SnakeAndLadder {
+	
+	public static void main(String []args) {
+		
+		double dice = Math.floor(Math.random() * 10) % 7;
+		
+		System.out.println("Player roll a dice");
+		
+		System.out.println("Dice number is : " + dice);
+	}
 
 }
