8511a95c27fbbfc4c01ac2e88e133a82bb4b4bbd
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-28 22:38:30 +0530

    Added: UC6 file is added successfully

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,7 +6,9 @@ public class SnakeAndLadder {
 		
 		int dicepos=0;
 		
-		for( dicepos=1; dicepos<=100; dicepos++) {
+		int count=0;
+		
+		for( dicepos=1; dicepos<100; dicepos++) {
 		
 		int dice = (int) Math.floor(Math.random() * 10) % 7;
 
@@ -15,56 +17,64 @@ public class SnakeAndLadder {
 		case 1:
 			System.out.println("Ladder");
 			dicepos = dicepos + dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 			
 			break;
 			
 		case 2:
 			System.out.println("Ladder");
 			dicepos = dicepos + dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 			
 			break;
 			
 		case 3:
 			System.out.println("Ladder");
 			dicepos = dicepos + dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 		    
 			break;
 		    
 		case 4:
 			System.out.println("Snake");
 			dicepos = dicepos - dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 		
 			break;
 			
 		case 5:
 			System.out.println("Snake");
 			dicepos = dicepos - dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 			
 			break;
 			
 		case 6:
 			System.out.println("Snake");
 			dicepos = dicepos - dice;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 			
 			break;
 			
 		default:
 			System.out.println("No Moves");
 			dicepos = dicepos;
-			System.out.println(dicepos);
+			System.out.println("dice position:"+dicepos);
 			
 			break;
 		}
 		
 		//dicepos = dicepos+dice;
+		if(dicepos > 100) {
+			dicepos = 100-dicepos;
+			System.out.println("dice position:"+dicepos);
+		}
+		
+		count++;
 		
 		}
+		System.out.println("No. of times the die rolled = "+count);
 	}
+	
 
 }
