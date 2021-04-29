af1b2d6f6b0cfa50cf365ef3db5ee6a592d435ac
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-29 20:08:02 +0530

    Added: UC7 successfully added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,75 +3,112 @@ public class SnakeAndLadder {
 	
 	public static void main(String []args) {
 		
+		int dicepos1=0;
+		int dicepos2=0;
+		int start=0;
+		int lastpos=100;
+		int count1=0;
+		int count2=0;
 		
-		int dicepos=0;
-		
+		while(dicepos1 < lastpos && dicepos2 < lastpos) {
 			
-		for( dicepos=1; dicepos<100; dicepos++) {
-		
-		int dice = (int) Math.floor(Math.random() * 10) % 7;
-		
-		
-		switch (dice) {
-		
-		case 1:
-			System.out.println("Ladder");
-			dicepos = dicepos + dice;
-			System.out.println("dice position:"+dicepos);
 			
-			break;
+			int dice1 = (int) Math.floor(Math.random() * 10) % 7;    //1 or 2 or 3 or 4 or 5 or 6
 			
-		case 2:
-			System.out.println("Ladder");
-			dicepos = dicepos + dice;
-			System.out.println("dice position:"+dicepos);
+			int option = (int) Math.floor(Math.random() * 10) % 3;   //1 or 2
 			
-			break;
+			switch (option) {
 			
-		case 3:
-			System.out.println("Ladder");
-			dicepos = dicepos + dice;
-			System.out.println("dice position:"+dicepos);
-			
-			break;
-		    
-		case 4:
-			System.out.println("Snake");
-			dicepos = dicepos - dice;
-			System.out.println("dice position:"+dicepos);
-		
-			break;
-			
-		case 5:
-			System.out.println("Snake");
-			dicepos = dicepos - dice;
-			System.out.println("dice position:"+dicepos);
+			case 0:
 				
-			break;
+				System.out.println("No Moves");
+				dicepos1=dicepos1;                         
+				System.out.println("player1 dice position: " +dicepos1);
+				break;
 			
-		case 6:
-			System.out.println("Snake");
-			dicepos = dicepos - dice;	
-			System.out.println("dice position:"+dicepos);
+			case 1:
 				
-			break;
+				System.out.println("Ladder");
+				dicepos1=dicepos1+dice1;                         
+				System.out.println("player1 dice position: " +dicepos1);
+				break;
+				
+			case 2:
+				
+				System.out.println("Snake");
+				dicepos1=dicepos1-dice1;
+				System.out.println("player1 dice position: " +dicepos1);
+				
+				break;
 			
-		default:
-			System.out.println("No Moves");
-			dicepos = dicepos;
-			//System.out.println("dice position:"+dicepos);
+			}
 			
-			break;
-		}
-		
-		}
-		
-		if(dicepos == 100) {
-			System.out.println("player won & dice position:"+dicepos);
-			
-		}
-		
-	}
-	
+			if(dicepos1 == 100) {
+				break;
+			}
 
+			else if(dicepos1 > 100) {
+				dicepos1 = dicepos1-dice1;
+				System.out.println("Invalid move by player1");
+			}
+			else if(dicepos1 < 0) {
+				dicepos1 = start;
+			}
+			
+			else {
+				System.out.println("new position: "+dicepos1);
+			}
+			
+			
+			int dice2 = (int) Math.floor(Math.random() * 10) % 7;    //1 or 2 or 3 or 4 or 5 or 6
+			
+			int option2 = (int) Math.floor(Math.random() * 10) % 3; 
+			
+			switch(option2) {
+			
+			case 0:
+				
+				System.out.println("No moves");
+				dicepos2=dicepos2;
+				break;
+				
+			case 1:
+				
+				System.out.println("Ladder");
+				dicepos2=dicepos2+dice2;
+				System.out.println("player2 dice position: "+dicepos2);
+				break;
+				
+			case 2:
+				
+				System.out.println("Snake");
+				dicepos2=dicepos2-dice2;
+				System.out.println("player2 dice position: "+dicepos2);
+				break;
+				
+			}
+				
+			if(dicepos2 == 100) {
+				break;
+				
+			}
+			
+			else if(dicepos2 > 100) {
+				dicepos2 = dicepos2-dice2;
+				System.out.println("Invalid move by player2");
+			}
+			else if(dicepos2 < 0) {
+				dicepos2 = start;
+			}
+			else {
+				System.out.println("new position: "+dicepos2);
+			}
+			
+		}	
+		if (dicepos1==100) {
+			System.out.println("Player1 Won");
+		}else {
+			System.out.println("Player2 Won");
+		}
+	}
 }
